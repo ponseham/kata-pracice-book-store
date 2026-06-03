0d5bcec69eeaf53e1f0673f02d1aebb561ef9a26
ponseham <ponseha94xgmail.com>
2026-06-03 14:08:39 +0530

    Show price without any discount when selecting single book

---
+++ src/components/BasketSummary.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from 'react-redux'
+import {
+    SUBTOTAL_LABEL,
+    TOTAL_LABEL,
+    CURRENCY_LABEL,
+    CSS_CLASSES,
+    EMPTY_COUNT,
+} from '../constants/uiConstants'
+import { selectBasketPrice } from '../store/selectors'
+
+export default function BasketSummary() {
+    const pricingSummery = useSelector(selectBasketPrice)
+
+    return (
+        <>
+            <hr />
+            <div className={CSS_CLASSES.BASKET_PRICE_ROW}>
+                <span>{SUBTOTAL_LABEL}</span>
+                <span className={CSS_CLASSES.PRICE_AMOUNT}>{pricingSummery.subtotal.toFixed(2)} {CURRENCY_LABEL}</span>
+            </div>
+            <div className={CSS_CLASSES.BASKET_PRICE_ROW_TOTAL}>
+                <span>{TOTAL_LABEL}</span>
+                <span className={CSS_CLASSES.PRICE_AMOUNT}>{pricingSummery.total.toFixed(2)} {CURRENCY_LABEL}</span>
+            </div>
+        </>
+    )
+}

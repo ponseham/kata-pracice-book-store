3f2bf3c1f41d9b127a35d83f861824d66ad37e9a
ponseham <ponseha94xgmail.com>
2026-06-03 13:52:32 +0530

    When click remove button delete items from basket

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,19 +1,34 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import {
     CSS_CLASSES,
     TEST_ID_BASKET_ITEM,
+    REMOVE_BUTTON_LABEL,
+    REMOVE_BUTTON_AREA_LABEL
 } from '../constants/uiConstants'
 import { selectBookQuantity } from '../store/selectors'
 import '../styles.css'
+import { removeBookFromBasket } from '../store/actions'
 
 export default function BasketItem({ book }) {
     const quantity = useSelector(selectBookQuantity(book.id))
+    const dispatch = useDispatch()
+    function handleRemoveBook() {
+        dispatch(removeBookFromBasket(book.id))
+    }
+
     return (
         <div className={CSS_CLASSES.BASKET_ITEM} data-testid={TEST_ID_BASKET_ITEM}>
             <span>
                 {book.title}
                 <span className={CSS_CLASSES.QUANTITY_BADGE}>×{quantity}</span>
             </span>
+            <button
+                className={CSS_CLASSES.REMOVE_BUTTON_AREA}
+                aria-label={REMOVE_BUTTON_AREA_LABEL.replace('_', book.title)}
+                onClick={handleRemoveBook}
+            >
+                {REMOVE_BUTTON_LABEL}
+            </button>
         </div>
     )
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,11 @@
 export const ADD_BOOK_TO_BASKET = 'ADD_BOOK_TO_BASKET'
+export const REMOVE_BOOK_FROM_BASKET = 'REMOVE_BOOK_FROM_BASKET'
+
 export const addBookToBasket = (bookId) => ({
     type: ADD_BOOK_TO_BASKET,
+    payload: bookId,
+})
+export const removeBookFromBasket = (bookId) => ({
+    type: REMOVE_BOOK_FROM_BASKET,
     payload: bookId,
 })

6c32b86312ab73208258ce18535fe1ace35eac39
ponseham <ponseha94xgmail.com>
2026-06-03 13:58:53 +0530

    Show total count of books added in basket

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import { STORE_HEADER_TITLE, CSS_CLASSES } from '../constants/uiConstants'
+import { useSelector } from 'react-redux'
+import { STORE_HEADER_TITLE, CSS_CLASSES, IN_BASKET_SUFFIX } from '../constants/uiConstants'
+import { selectTotalItemCount, selectHeaderItemLabel } from '../store/selectors'
+
 function Header() {
+    const totalItemCount = useSelector(selectTotalItemCount)
+    const itemLabel = useSelector(selectHeaderItemLabel)
+
     return (
         <div className={CSS_CLASSES.HEADER}>
             <div className={CSS_CLASSES.HEADER_CONTAINER}>
                 <h1>{STORE_HEADER_TITLE}</h1>
+                <span>{totalItemCount} {itemLabel} {IN_BASKET_SUFFIX}</span>
             </div>
         </div>
     )

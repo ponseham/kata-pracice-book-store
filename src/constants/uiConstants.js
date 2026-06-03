58ec9d049bf1db9c35e92ddf41017e860074e53c
ponseham <ponseha94xgmail.com>
2026-06-03 13:08:10 +0530

    Show all books information as card

--- src/constants/uiConstants.js
+++ src/constants/uiConstants.js
@@ -2,5 +2,14 @@ export const STORE_HEADER_TITLE = 'Books Store'
 
 export const CSS_CLASSES = {
     HEADER: 'header',
-    HEADER_CONTAINER: 'container-row'
+    HEADER_CONTAINER: 'container-row',
+    MAIN_COLUMN: 'main-column',
+    BOOK_GRID: 'book-grid',
+    BOOK_CARD: 'book-card',
+    BOOK_INFO: 'book-info',
+    BOOK_PRICE_ROW: 'book-price-row',
 }
+
+export const TEST_ID_BOOK_CARD = 'book-card'
+
+export const CURRENCY_LABEL = 'EUR'
